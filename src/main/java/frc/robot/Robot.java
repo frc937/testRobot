9cf7884ece572bbf9f3b58521b4e7a4658d01f70
Oliver <3086549@smsd.org>
2024-11-18 15:56:05 -0600

    print Hello World

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -16,6 +16,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
+    System.out.println("Hello World");
   }
 
   @Override
