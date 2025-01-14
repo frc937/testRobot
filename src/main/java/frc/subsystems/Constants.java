b866ec512846bc01d3f9c01744e8df905acfec25
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2025-01-14 16:55:39 -0600

    create constants and add talon id constants

---
+++ src/main/java/frc/subsystems/Constants.java
@@ -0,0 +1,15 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.subsystems;
+
+/** Add your docs here. */
+public final class Constants {
+
+    /* Drive motor can IDs */
+    public static final int ID_TALON_FRONT_LEFT = 0;
+    public static final int ID_TALON_FRONT_RIGHT = 0;
+    public static final int ID_TALON_BACK_LEFT = 0;
+    public static final int ID_TALON_BACK_RIGHT = 0;
+}
