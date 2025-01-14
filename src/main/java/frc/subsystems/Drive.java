3db22b8fddaf3d5470f0279a9fca65e79c73be48
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2025-01-14 16:30:12 -0600

    indent formatting change

--- src/main/java/frc/subsystems/Drive.java
+++ src/main/java/frc/subsystems/Drive.java
@@ -7,11 +7,12 @@ package frc.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drive extends SubsystemBase {
-  /** Creates a new Drive. */
-  public Drive() {}
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    public Drive() {
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
 }
