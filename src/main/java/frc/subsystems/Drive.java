d4f9d4225ca123e8208b8f3548f2a2d186ead63b
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2025-01-15 08:18:43 -0600

    constants isnt supposed to be in subsystems :facepalm:

--- src/main/java/frc/subsystems/Drive.java
+++ src/main/java/frc/subsystems/Drive.java
@@ -9,6 +9,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.Constants;
 
 public class Drive extends SubsystemBase {
 
